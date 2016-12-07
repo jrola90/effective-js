86e3eaa3892dd0cf6bddcf5f04e880c24fb8659f
jrola90 <jartsw@gmail.com>
2016-12-07 01:15:27 +0100

    Eval function

---
+++ item16/scripts/script2.js
@@ -0,0 +1,12 @@
+var y = "globalna";
+function test(x) {
+    if (x) {
+        (function () {
+            eval("var y = 'lokalna';"); // Wi¹zanie dynamiczne
+        })();
+    }
+    return y;
+}
+
+console.log(test(true));
+console.log(test(false));
